18819b35e3cb25bbd6bcc3518cbd9f5805a7a1d7
2411186484xy-hash <2411186484xy-hash@users.noreply.github.com>
2026-09-21 21:42:54 +0800

    P1.1: os.ps1 requires PS 5.1 for sandbox compat

--- os.ps1
+++ os.ps1
@@ -1,4 +1,4 @@
-#Requires -Version 7.0
+#Requires -Version 5.1
 <# Idea OS v2 single entry. Usage: .\os.ps1 <command> [args...] #>
 $ErrorActionPreference = "Stop"
 $Repo = Split-Path -Parent $MyInvocation.MyCommand.Path
